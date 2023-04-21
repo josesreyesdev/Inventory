7a5d57afcd510c746ecabbcc48dc648125e7ff7a
Jose SR <josesanchez09190@gmail.com>
2023-04-20 23:46:46 -0600

    upgrade plugins

--- app/src/main/java/com/devjsr/inventario/ui/AddItemFragment.kt
+++ app/src/main/java/com/devjsr/inventario/ui/AddItemFragment.kt
@@ -30,7 +30,7 @@ class AddItemFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
         _binding = FragmentAddItemBinding.inflate(inflater, container, false)
 
--- app/src/main/java/com/devjsr/inventario/ui/ItemDetailFragment.kt
+++ app/src/main/java/com/devjsr/inventario/ui/ItemDetailFragment.kt
@@ -33,7 +33,7 @@ class ItemDetailFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
         _binding = FragmentItemDetailBinding.inflate( inflater, container, false)
         return binding.root
